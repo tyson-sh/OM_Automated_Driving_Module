d90d6e60e3d360bb88057fddf6c32695361a9139
t-shields <iffy.dude@gmail.com>
2020-08-19 19:00:38 +1000

    Flesh out PID Controller logic

--- OM_Automated_Driving/Controller.cs
+++ OM_Automated_Driving/Controller.cs
@@ -2,6 +2,51 @@ namespace OM_Automated_Driving
 {
     public class Controller
     {
-        // Just a shell for MVP
+        // New value of the derivative gain
+        public float KProportional;
+        
+        // New value of the integral gain
+        public float KIntegral;
+        
+        // New value of the proportional gain
+        public float KDerivative;
+
+        private float ErrorOld;
+        private float ErrorSum;
+
+        /// <summary>
+        ///     PID control algorithm for controlling signals
+        /// </summary>
+        /// <param name="SetPoint">Value we are trying to reach</param>
+        /// <param name="ProcessVar">Current Value</param>
+        /// <param name="TimeStep">Elapsed time since last update</param>
+        /// <returns>Computed PID controller value</returns>
+        public float Control(float SetPoint, float ProcessVar, float TimeStep)
+        {
+            // Dimension all variables local to this routine
+            float Derivative;
+            float Integral;
+            float SigError;
+            
+            // Calculate the error between the target and the driver
+            SigError = SetPoint - ProcessVar;
+            
+            // Compute the integral and derivative terms
+            ErrorSum = ErrorSum + SigError;
+            Integral = ErrorSum * TimeStep;
+            Derivative = (SigError - ErrorOld) / TimeStep;
+            ErrorOld = SigError;
+            
+            // Return the new control value
+            return KProportional * SigError + KIntegral * Integral + KDerivative * Derivative;
+        }
+        
+        /// <summary>
+        ///     Method that resets the previous error value
+        /// </summary>
+        public void ResetDV()
+        {
+            ErrorOld = 0;
+        }
     }
 }
